d64921aaec08e6b2e4d5ba5825b835cfd45fe8aa
pivendanila <piven.danila@mail.ru>
2023-04-28 17:42:14 +0300

    feat: bugs fixed

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,13 @@ import java.nio.file.Path;
 public class Main {
     public static void main(String[] args) {
         CollectionManager manager = new CollectionManager();
-        manager.fill_from_file(Path.of(args[0]));
-        Executor exec = new Executor(manager);
-        exec.interactiveMod();
+        if(args.length == 0){
+            System.out.println("Specify file as argument");
+        }
+        else {
+            manager.fill_from_file(Path.of(args[0]));
+            Executor exec = new Executor(manager);
+            exec.interactiveMod();
+        }
     }
 }
